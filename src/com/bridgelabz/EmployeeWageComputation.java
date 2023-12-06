2bff8d7a7a938e5ef1a0ec698d6db137b034b1f2
prakash-zodge-bridgelabz <prakash.zodge.bridgelabz@gmail.com>
2023-12-06 22:49:13 +0530

    Modified: Calculate employee wage using switch case

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -3,20 +3,21 @@ package com.bridgelabz;
 public class EmployeeWageComputation {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
-        int IS_PART_TIME = 1;
-        int IS_FULL_TIME = 2;
+        final int IS_PART_TIME = 1;
+        final int IS_FULL_TIME = 2;
         int EMP_RATE_PER_HOUR = 20;
         int empHrs = 0;
         int empWage = 0;
-        double empCheck = Math.floor(Math.random() * 10) % 3;
-        if(empCheck == IS_PART_TIME){
-            empHrs = 4;
-        }
-        else if(empCheck == IS_FULL_TIME){
-            empHrs = 8;
-        }
-        else{
-            empHrs = 0;
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+        switch (empCheck){
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
+            case IS_FULL_TIME:
+                empHrs = 8;
+                break;
+            default:
+                empHrs = 0;
         }
         empWage = empHrs * EMP_RATE_PER_HOUR;
         System.out.println("Employee Wage : "+empWage);
